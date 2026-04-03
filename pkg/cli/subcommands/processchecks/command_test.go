aa8172d398d0787bc1ab33a4ac84e3b9d33201b1
louis-cqrl <93274433+louis-cqrl@users.noreply.github.com>
2026-04-03 03:05:09 +0000

    fix(lint): use npcollector mock package instead of npcollectorimpl.MockModule

--- pkg/cli/subcommands/processchecks/command_test.go
+++ pkg/cli/subcommands/processchecks/command_test.go
@@ -31,7 +31,7 @@ import (
 	workloadfilterfxmock "github.com/DataDog/datadog-agent/comp/core/workloadfilter/fx-mock"
 	workloadmeta "github.com/DataDog/datadog-agent/comp/core/workloadmeta/def"
 	workloadmetafxmock "github.com/DataDog/datadog-agent/comp/core/workloadmeta/fx-mock"
-	npcollectorimpl "github.com/DataDog/datadog-agent/comp/networkpath/npcollector/impl"
+	npcollectormock "github.com/DataDog/datadog-agent/comp/networkpath/npcollector/mock"
 	processComponent "github.com/DataDog/datadog-agent/comp/process"
 	rdnsquerierfxmock "github.com/DataDog/datadog-agent/comp/rdnsquerier/fx-mock"
 	proccontainers "github.com/DataDog/datadog-agent/pkg/process/util/containers"
@@ -70,7 +70,7 @@ func TestCommand(t *testing.T) {
 			workloadfilterfxmock.MockModule(),
 			fx.Provide(func() tagger.Component { return taggerfxmock.SetupFakeTagger(t) }),
 			rdnsquerierfxmock.MockModule(),
-			npcollectorimpl.MockModule(),
+			npcollectormock.MockModule(),
 			processComponent.Bundle(),
 
 			// InitSharedContainerProvider must be called before the application starts so the workloadmeta collector can be initialized correctly.
