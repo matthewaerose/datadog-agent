7ef2baa4a01d223adab3aa74a0bd1db3245b1e83
Bryce Kahle <bryce.kahle@datadoghq.com>
2026-04-02 17:13:23 -0700

    save fixes

--- pkg/network/ebpf/c/sk/defs.h
+++ pkg/network/ebpf/c/sk/defs.h
@@ -8,36 +8,32 @@
 #endif
 
 typedef struct {
+    conn_tuple_t tup;
+
     __u64 initial_sent_bytes;
     __u64 initial_recv_bytes;
     __u32 initial_sent_packets;
     __u32 initial_recv_packets;
     __u32 initial_retransmits;
-
-    __u32 pid;
     __u16 state_transitions;
     __u16 failure_reason;
-//    __u32 cookie;
 
     tcp_event_stats_t tcp_event_stats;
-
     time_ms_t start_ms;
     __u8 direction;
-    conn_tuple_t tup;
 } sk_tcp_stats_t;
 
 typedef struct {
+    conn_tuple_t tup;
+
     __u64 sent_bytes;
     __u64 recv_bytes;
     __u32 sent_packets;
     __u32 recv_packets;
-    __u32 pid;
-//    __u32 cookie;
-    time_ms_t start_ms;
-    time_ms_t timestamp_ms;
     __u8 flags;
     __u8 direction;
-    conn_tuple_t tup;
+    time_ms_t timestamp_ms;
+    time_ms_t start_ms;
 } sk_udp_stats_t;
 
 #endif
