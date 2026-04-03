48e920577b7a6c12cb8cdf839e3ef97481b23f0b
louis-cqrl <93274433+louis-cqrl@users.noreply.github.com>
2026-04-03 14:35:01 +0200

    fix: use submitter mock package instead of impl in agent_test.go

--- comp/process/agent/agentimpl/agent_test.go
+++ comp/process/agent/agentimpl/agent_test.go
@@ -25,7 +25,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/process/hostinfo/hostinfoimpl"
 	"github.com/DataDog/datadog-agent/comp/process/processcheck/processcheckimpl"
 	"github.com/DataDog/datadog-agent/comp/process/runner/runnerimpl"
-	"github.com/DataDog/datadog-agent/comp/process/submitter/impl"
+	submittermock "github.com/DataDog/datadog-agent/comp/process/submitter/mock"
 	"github.com/DataDog/datadog-agent/pkg/util/flavor"
 	"github.com/DataDog/datadog-agent/pkg/util/fxutil"
 )
@@ -68,7 +68,7 @@ func TestProcessAgentComponent(t *testing.T) {
 			opts := []fx.Option{
 				runnerimpl.Module(),
 				hostinfoimpl.MockModule(),
-				submitterimpl.MockModule(),
+				submittermock.MockModule(),
 				taggerfxmock.MockModule(),
 				statsdimpl.MockModule(),
 				Module(),
